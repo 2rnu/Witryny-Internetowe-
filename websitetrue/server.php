8b9ad7470174db8eb7666b2130a2e2183dec9919
Gabriel Achler <112950410+2rnu@users.noreply.github.com>
2024-04-05 15:06:24 +0200

    Add files via upload

---
+++ websitetrue/server.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+// initializing variables
+$username = "root";
+$errors = array(); 
+
+// connect to the database
+$db = mysqli_connect('localhost', $username, '', 'użytkownicy');
+
+// LOGIN USER
+if (isset($_POST['login_user'])) {
+  $username = mysqli_real_escape_string($db, $_POST['nazwa_uzytkownika']);
+  $password = mysqli_real_escape_string($db, $_POST['haslo']);
+
+  if (empty($username)) {
+  	array_push($errors, "Username is required");
+  }
+  if (empty($password)) {
+  	array_push($errors, "Password is required");
+  }
+
+  if (count($errors) == 0) {
+  	$password = md5($password);
+  	$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+  	$results = mysqli_query($db, $query);
+  	if (mysqli_num_rows($results) == 1) {
+  	  $_SESSION['username'] = $username;
+  	  $_SESSION['success'] = "You are now logged in";
+  	  header('location: index.php');
+  	}else {
+  		array_push($errors, "Wrong username/password combination");
+  	}
+  }
+}
+
+?>
